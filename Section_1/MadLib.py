16feca646d14960f5f5003bf4b2ae799152185df
Sloane Nelson <sloane@Jeffs-Mac-mini.local>
2023-09-19 19:37:41 -0500

    initial commit

---
+++ Section_1/MadLib.py
@@ -0,0 +1,10 @@
+# MadLib.py
+adjective = input("Please enter an adjective: ")
+noun = input("Please enter a noun: ")
+verb = input("Please enter a verb ending in -ed: ")
+adverb1 = input("Please enter an adverb: ")
+noun1 = input("Please enter a noun: ")
+verb1 = input("Please enter a verb ending in -ing: ")
+print("Your MadLib:")
+print("The", adjective, noun, verb, "over the lazy brown dog.")
+print("She saw the", adverb1, noun1, verb1, "off the building.")
---
+++ Section_1/my_name.py
@@ -0,0 +1,5 @@
+# YourName.py
+first_name = input("What is your first name?\n")
+last_name = input("What is your last name?\n")
+full_name = first_name + " " + last_name + " "
+print("Hi, your full name is", full_name * 10000) 
